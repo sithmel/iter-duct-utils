3db1dff7aade4684244bdfa09ba5c259baf26314
Denis Fernandez Gridchin <denis.fdez@gmail.com>
2018-06-18 18:30:28 +0200

    Fixed map test

--- test/map.test.js
+++ test/map.test.js
@@ -7,6 +7,6 @@ describe('map', () => {
   it('input object equals mapped', async () => {
     const mapper = map({ func: (item) => item * item })
     const squares = await itools.asyncIterToArray(mapper([1, 2, 3]))
-    assert.equal(squares, [1, 4, 9])
+    assert.deepEqual(squares, [1, 4, 9])
   })
 })
