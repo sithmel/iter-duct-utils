4cc5a1b689adba6892ff65dc5ad088393f13f013
Maurizio Lupo <maurizio.lupo@gmail.com>
2018-06-18 12:13:20 +0100

    a couple of minor fixes

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const logger = require('./logger')
 const mongoReader = require('./mongo-reader')
 const download = require('./download')
+const csvReader = require('./csv-reader')
+const filter = require('./filter')
+const map = require('./map')
+const slice = require('./slice')
+const jsonReader = require('./json-reader')
+const jsonWriter = require('./json-writer')
+
 
 module.exports = {
   logger,
   mongoReader,
-  download
+  download,
+  csvReader,
+  filter,
+  map,
+  slice,
+  jsonReader,
+  jsonWriter
 }
--- throttle.js
+++ throttle.js
@@ -8,4 +8,5 @@ function throttle(cfg) {
     return iterable
   }
 }
+
 module.exports = throttle
